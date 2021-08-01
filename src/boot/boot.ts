bb7bd04e1de43504389838e9d3ec3233b51e8559
Güven ALTUNSOY <guven@shopihq.com>
2021-08-01 15:34:55 +0300

    request response edited

--- src/boot/boot.ts
+++ src/boot/boot.ts
@@ -21,6 +21,12 @@ const registerCoreMiddleware = function (): void {
 
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
+        app.use((req, res, next) => {
+            res.append("Access-Control-Allow-Origin", ["*"]);
+            res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+            res.append("Access-Control-Allow-Headers", "Content-Type");
+            next();
+        });
         logger.info(`BOOT :: Registered middleware : bodyParser`);
 
         app.use(cors());
